5dea4b2049dbdabda565b6f16c70afec959a100f
James Jiang <monochrome473@gmail.com>
2020-03-19 21:33:48 +0000

    Automatically backed up by Learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,5 +1,2 @@
 puts "Welcome to Tic Tac Toe!"
-ruby welcome.#!/usr/bin/env ruby -wKU
-
-# You can see what this file does by running:
-# ruby lib/welcome.rb from your terminal.
+ruby lib/welcome.rb
